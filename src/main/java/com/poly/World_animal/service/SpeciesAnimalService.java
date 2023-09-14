b25bf866e9b53c309e523fe4ee89aad54ddc5a53
NguyenChiThanh <101045303+ncthanh2001@users.noreply.github.com>
2023-09-14 15:45:54 +0700

    home_feature v1

--- src/main/java/com/poly/World_animal/service/SpeciesAnimalService.java
+++ src/main/java/com/poly/World_animal/service/SpeciesAnimalService.java
@@ -2,9 +2,11 @@ package com.poly.World_animal.service;
 
 
 import com.poly.World_animal.entity.SpeciesAnimal;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface SpeciesAnimalService {
-    List<SpeciesAnimal> findAll();
+   Page<SpeciesAnimal> findAll(Pageable page);
 }
--- src/main/java/com/poly/World_animal/service/impl/SpeciesServiceImpl.java
+++ src/main/java/com/poly/World_animal/service/impl/SpeciesServiceImpl.java
@@ -5,6 +5,8 @@ import com.poly.World_animal.repository.OrderAnimalRepo;
 import com.poly.World_animal.repository.SpeciesAnimalRepo;
 import com.poly.World_animal.service.SpeciesAnimalService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,8 +16,9 @@ public class SpeciesServiceImpl implements SpeciesAnimalService {
 
     @Autowired
     SpeciesAnimalRepo speciesAnimalRepo ;
+
     @Override
-    public List<SpeciesAnimal> findAll() {
-        return speciesAnimalRepo.findAll();
+    public Page<SpeciesAnimal> findAll(Pageable page) {
+        return speciesAnimalRepo.findAll(page);
     }
 }
